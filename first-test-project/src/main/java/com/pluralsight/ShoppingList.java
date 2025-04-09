272508fae6d9a7e41072572c37494273e26098d0
Maksim Koshkin <mkoshkin@my.yearupunited.org>
2025-04-09 11:54:57 -0400

    comleted SgoppingList app

---
+++ first-test-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ShoppingList {
+    public static void main(String[] args) {
+System.out.println("milk bread corn cheese apple orange banana soda water sugar");
+        }
+    }
