bfdde989e7df53ce147f53ac6ee7db46a5f193a3
Maksim Koshkin <mkoshkin@my.yearupunited.org>
2025-04-09 14:01:09 -0400

    completed ShopingList app

--- first-test-project/src/main/java/com/pluralsight/ShoppingList.java
+++ first-test-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -4,6 +4,15 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ShoppingList {
     public static void main(String[] args) {
-System.out.println("milk bread corn cheese apple orange banana soda water sugar");
+System.out.println("milk");
+        System.out.println("bread");
+        System.out.println("corn");
+        System.out.println("cheese");
+        System.out.println("aplle");
+        System.out.println("orange");
+        System.out.println("banana");
+        System.out.println("soda");
+        System.out.println("water");
+        System.out.println("eggs");
         }
     }
