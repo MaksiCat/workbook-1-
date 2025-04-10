ace8ee8e9e5c7a32bbd8be5874bea4705d0b97fb
Maksim Koshkin <mkoshkin@my.yearupunited.org>
2025-04-10 10:18:48 -0400

    completed Mod3 Exercise 2 - MathApp

---
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,27 @@
+public class MathApp {
+    public static void main(String[] args) {
+       // Question 1
+        int bobSalary = 10000;
+        int garySalary = 5000;
+        int highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is" + highestSalary);
+        // Question 2
+        int carPrice = 20000;
+        int truckPrice = 40000;
+        int smallestPrice = Math.min(carPrice, truckPrice);
+        System.out.println("Smallest price is" + smallestPrice);
+        // Question 3
+        double radios = 7.25;
+        double area = Math.PI * Math.pow(radios, 2);
+        System.out.println("The area of the circle is" + area);
+        // Question 3
+        double number = 5.0;
+        double squareRoot = Math.sqrt(number);
+        System.out.println("The ansver is" + squareRoot);
+        // Question 4
+        int fdistance = 5;
+        int sdistance = 10;
+        int tdistance = 85;
+        int foDistance = 50
+    }
+}
